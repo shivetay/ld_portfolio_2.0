8f1206d1274bd9d98448ab8ed314990f30dd5510
Łukasz Dawidowicz <lddev@outlook.com>
2020-06-01 13:44:40 +0200

    add user and project validation

---
+++ middleware/validation.js
@@ -0,0 +1,34 @@
+exports.userValidation = (req, res, next) => {
+  [
+    // validation
+    req.check('name', 'Name is requierd').notEmpty(),
+    req.check('email', 'Add valid email').isEmail().notEmpty(),
+    req
+      .check('password', 'Password requier min 6 or more characters')
+      .isLength({
+        min: 6,
+      })
+      .notEmpty(),
+    // .matches(/\d/)
+  ];
+  const errors = req.validationErrors();
+  if (errors) {
+    const firstError = errors.map((error) => error.msg)[0];
+    return res.status(400).json({ error: firstError });
+  }
+  next();
+};
+
+exports.projectValidation = (req, res, next) => {
+  [
+    //validation
+    req.check('title', 'Title is required').not().isEmpty(),
+    req.check('description', 'Description is required').not().isEmpty(),
+  ];
+  const errors = req.validationErrors();
+  if (errors) {
+    const firstError = errors.map((error) => error.msg)[0];
+    return res.status(400).json({ error: firstError });
+  }
+  next();
+};
