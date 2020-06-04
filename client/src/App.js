a3bad411d5f7c8233c9b06435fb27fa0802a6aa5
Łukasz Dawidowicz <lddev@outlook.com>
2020-06-04 12:26:27 +0200

    init frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>App.js</h1>
-      </div>
-    );
-  }
-}
+import MainLayout from './components/layout/MainLayout/MainLayout';
+import Home from './components/views/Home/Home';
+
+const App = () => {
+  return (
+    // <Provider store={}>
+    <Fragment>
+      <BrowserRouter>
+        <MainLayout>
+          <Switch>
+            <Route exact path='/' component={Home} />
+          </Switch>
+        </MainLayout>
+      </BrowserRouter>
+    </Fragment>
+    // </Provider>
+  );
+};
 
 export default App;
