e850b14949fb052db243aa9de7eed79d214c1d52
Łukasz Dawidowicz <lddev@outlook.com>
2020-06-12 17:08:53 +0200

    prep admin dashboard

--- client/src/components/views/AdminDashboard/AdminDashboard.js
+++ client/src/components/views/AdminDashboard/AdminDashboard.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { isAuthUser } from '../../../utils/utils';
+
+import Layout from '../../layout/MainLayout/Layout';
 
 const AdminDashboard = () => {
-  return <div>admin</div>;
+  const {
+    user: { _id, name, email, role },
+  } = isAuthUser();
+
+  const adminLinks = () => {
+    return (
+      <div className=''>
+        <h4 className=''>Admin Links</h4>
+        <ul className=''>
+          <li className='list-group-item'>
+            <Link to={`/projects/create/${_id}`} className=''>
+              Create Project
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+
+  return (
+    <Layout
+      title='Admin Dashboard'
+      description={`Wlecome ${name}`}
+      className=''>
+      <div className=''>
+        <div className=''>{adminLinks()}</div>
+        <div className=''></div>
+      </div>
+    </Layout>
+  );
 };
 
 export default AdminDashboard;
