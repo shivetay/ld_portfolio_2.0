3537c669bebe16bd4d88ca00bfd77881a0920945
Łukasz Dawidowicz <lddev@outlook.com>
2020-07-16 11:41:44 +0200

    add pagination

--- client/src/components/views/Projects/Projects.js
+++ client/src/components/views/Projects/Projects.js
@@ -12,15 +12,29 @@ class Projects extends Component {
   state = {
     data: [],
     loading: false,
+    page: 1,
   };
   componentDidMount() {
-    this.getProjects();
+    const { page } = this.state;
+    this.getProjects(page);
   }
 
-  getProjects = async () => {
+  loadMore = (number) => {
+    const { page } = this.state;
+    this.setState({ page: page + number });
+    this.getProjects(page + number);
+  };
+
+  loadLess = (number) => {
+    const { page } = this.state;
+    this.setState({ page: page - number });
+    this.getProjects(page - number);
+  };
+
+  getProjects = async (page) => {
     this.setState({ loading: true });
     try {
-      await axios.get(`${API_URL}/projects`).then((res) =>
+      await axios.get(`${API_URL}/projects?page=${page}`).then((res) =>
         this.setState({
           data: res.data,
           loading: false,
@@ -45,7 +59,32 @@ class Projects extends Component {
   };
 
   render() {
-    return <Fragment>{this.renderProjects()}</Fragment>;
+    const { data, page } = this.state;
+    return (
+      <Fragment>
+        <h2 className='mt-5 mb-5'>{!data.length ? 'No more projects!' : ''}</h2>
+        {this.renderProjects()}
+        {page > 1 ? (
+          <button
+            className='btn btn-raised btn-warning mr-5 mt-5 mb-5'
+            onClick={() => this.loadLess(1)}>
+            Previous ({this.state.page - 1})
+          </button>
+        ) : (
+          ''
+        )}
+
+        {data.length ? (
+          <button
+            className='btn btn-raised btn-success mt-5 mb-5'
+            onClick={() => this.loadMore(1)}>
+            Next ({page + 1})
+          </button>
+        ) : (
+          ''
+        )}
+      </Fragment>
+    );
   }
 }
 
