e8ce39738785312ee7ad78ae41c298a6fcbe30a6
Łukasz Dawidowicz <lddev@outlook.com>
2020-07-24 11:46:12 +0200

    add contact page

---
+++ client/src/components/views/Contact/Contact.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import './Contact.scss';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import {
+  faDiceFive,
+  faEnvelope,
+  faFilePdf,
+} from '@fortawesome/free-solid-svg-icons';
+
+const Contact = () => {
+  return (
+    <section className='Contact'>
+      <div className='Contact__Container'>
+        <div className='Contact__Content'>
+          <div className='Contact__Content-social'>
+            <div className='icon-group'>
+              {' '}
+              <a
+                target='_blank'
+                rel='noopener noreferrer'
+                href='https://www.linkedin.com/in/lukaszdawidowicz/'>
+                linkedin
+              </a>
+              <FontAwesomeIcon icon={faLinkedin} />
+            </div>
+            <div className='icon-group'>
+              <a
+                target='_blank'
+                rel='noopener noreferrer'
+                href='https://github.com/shivetay'>
+                github
+              </a>
+              <FontAwesomeIcon icon={faGithub} />
+            </div>
+            <div className='icon-group'>
+              <a
+                target='_blank'
+                rel='noopener noreferrer'
+                href='https://www.fiverr.com/shivetay?public_mode=true'>
+                fiver
+              </a>
+              <FontAwesomeIcon icon={faDiceFive} />
+            </div>
+          </div>
+          <div className='Contact__Content-rest'>
+            <span className='icon-group'>
+              <a href='#'>dawidowicz_lukasz@hotmail.com</a>
+              <FontAwesomeIcon icon={faEnvelope} />
+            </span>
+            <div className='icon-group'>
+              <a
+                target='_blank'
+                rel='noopener noreferrer'
+                href='https://1drv.ms/b/s!AmWwIHoCT8_JkXrJhBgVahZTF6iw?e=tXHiy1'>
+                CV
+              </a>
+              <FontAwesomeIcon icon={faFilePdf} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    //
+  );
+};
+
+export default Contact;
