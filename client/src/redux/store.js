06a2d5ab80d9bdc6a28fd69556bd23f54804a2da
Łukasz Dawidowicz <lddev@outlook.com>
2020-08-31 18:43:20 +0200

    prep for deploy

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-// import { composeWithDevTools } from 'redu-devtools-extension';
+import { composeWithDevTools } from 'redu-devtools-extension';
 import thunk from 'redux-thunk';
 
 import alerts from './AlertReducer';
@@ -30,10 +30,11 @@ const combinedReducers = combineReducers(reducers);
 const store = createStore(
   combinedReducers,
   initialState,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeWithDevTools(applyMiddleware(thunk))
+  // compose(
+  //   applyMiddleware(thunk),
+  //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // )
 );
 
 export default store;
